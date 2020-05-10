838bb2a59bbf4f3dacc4bc85bcaf2909f443b067
cindy951357 <cindy951357@gmail.com>
2020-05-11 00:41:53 +0800

    can show rabbit images but cannot scroll normally.

--- app/src/main/java/com/example/demorecyclerview/MainActivity.java
+++ app/src/main/java/com/example/demorecyclerview/MainActivity.java
@@ -3,10 +3,7 @@ package com.example.demorecyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -15,43 +12,43 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private final static int defaultId = -1;
     private static final String TAG = "MainActivity";
-    ArrayList<String> mImageNames = new ArrayList<String>();;
-    ArrayList<Integer> mImageIds = new ArrayList<Integer>();;
+    private int repeatTimes;
+    ArrayList<String> mImageNames = new ArrayList<String>();
+    ArrayList<Integer> mImageIds = new ArrayList<Integer>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d(TAG,"MainActivity called");
+        initRepeatTimes();
         initImageNamesAndIds();
         initRecyclerViewListAdapter();
-        Log.d(TAG,"layout manager is set?");
+    }
+
+    private void initRepeatTimes() {
+        repeatTimes = getResources().getInteger(R.integer.repeat_times);
     }
 
     private void initImageNamesAndIds(){
         Log.d(TAG,"initImageNamesAndIds called");
         TypedArray imgRabbitResourceArr = getResources().obtainTypedArray(R.array.img_rabbit);
-        for(int i=0; i<imgRabbitResourceArr.length(); i++) {
-            int imgId = imgRabbitResourceArr.getResourceId(i, defaultId);
-            mImageIds.add(imgId);
-            mImageNames.add(getResources().getResourceEntryName(imgId));
+        for(int j=0; j<repeatTimes; j++) {
+            for (int i = 0; i < imgRabbitResourceArr.length(); i++) {
+                int imgId = imgRabbitResourceArr.getResourceId(i, defaultId);
+                String imgShortFName = getResources().getResourceEntryName(imgId);
+                mImageIds.add(imgId);
+                mImageNames.add(imgShortFName);
+            }
         }
     }
 
     private void initRecyclerViewListAdapter(){
         Log.d(TAG,"initRecyclerViewListAdapter called. initiating adapter");
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view); /**/
-        Log.d(TAG,"01");
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         RecyclerViewListAdapter recyclerViewListAdapter = new RecyclerViewListAdapter(this, mImageNames, mImageIds);
-        Log.d(TAG,"02");
-
         recyclerView.setAdapter(recyclerViewListAdapter);
-        Log.d(TAG,"03");
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        Log.d(TAG,"04");
-
     }
-
 }
 
 /*Android Official Docs on TypedArray*/
--- app/src/main/java/com/example/demorecyclerview/RecyclerViewListAdapter.java
+++ app/src/main/java/com/example/demorecyclerview/RecyclerViewListAdapter.java
@@ -3,14 +3,12 @@ package com.example.demorecyclerview;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -19,6 +17,7 @@ public class RecyclerViewListAdapter extends RecyclerView.Adapter<RecyclerViewLi
     /* Nicole taught me that <> contains self-defined class*/
 
     private static final String TAG = "RecyclerViewListAdapter";
+    private static final int repeatTimes = R.integer.repeat_times;
 
     public RecyclerViewListAdapter(Context context, ArrayList<String> imageNames, ArrayList<Integer> imageIds) {
         mImageNames = imageNames;
@@ -30,7 +29,6 @@ public class RecyclerViewListAdapter extends RecyclerView.Adapter<RecyclerViewLi
     private ArrayList<Integer> mImageIds = new ArrayList<Integer>();
     private Context mContext;
 
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -39,12 +37,11 @@ public class RecyclerViewListAdapter extends RecyclerView.Adapter<RecyclerViewLi
         return holder;
     }
 
-
     @NonNull
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Log.d(TAG,"onBindViewHolder called.");
-        // - get element from the dataset at this position
+        /* get element from the dataset at this position */
         Log.d(TAG,mImageIds.get(position).toString());
         holder.imageRabbit.setImageResource(mImageIds.get(position));
         holder.imageName.setText(mImageNames.get(position));
@@ -52,13 +49,14 @@ public class RecyclerViewListAdapter extends RecyclerView.Adapter<RecyclerViewLi
 
     @Override
     public int getItemCount() {
-        return 4;/* cannot be 4 */
+        int repeatTimes = mContext.getResources().getInteger(R.integer.repeat_times);
+        return mImageIds.size()*repeatTimes;/* cannot be zero */
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView imageRabbit;
         TextView imageName;
-        ConstraintLayout parentLayout;
+        LinearLayout parentLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -68,5 +66,4 @@ public class RecyclerViewListAdapter extends RecyclerView.Adapter<RecyclerViewLi
 
         }
     }
-
 }
